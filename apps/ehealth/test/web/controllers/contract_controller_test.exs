f8e13ae49263b57601395cce90438a681f804f8e
Maksym Ivanov <imax.iva@gmail.com>
2018-07-17 12:58:27 +0300

    [minor] contract controller tests improved

--- apps/ehealth/test/web/controllers/contract_controller_test.exs
+++ apps/ehealth/test/web/controllers/contract_controller_test.exs
@@ -574,7 +574,7 @@ defmodule EHealth.Web.ContractControllerTest do
 
       conn =
         conn
-        |> put_client_id_header(UUID.generate())
+        |> put_client_id_header(legal_entity.id)
         |> put_consumer_id_header(party_user.user_id)
         |> Plug.Conn.put_req_header("drfo", party_user.party.tax_id)
         |> patch(contract_path(conn, :update, contract.id), params)
@@ -583,56 +583,7 @@ defmodule EHealth.Web.ContractControllerTest do
       assert "Division must be active and within current legal_entity" == resp["error"]["message"]
     end
 
-    test "contract and employee legal_entity_id mismatch", %{conn: conn} do
-      nhs()
-      contract_request = insert(:il, :contract_request)
-      contract = insert(:prm, :contract, contract_request_id: contract_request.id)
-      legal_entity = insert(:prm, :legal_entity, id: contract.contractor_legal_entity_id)
-      division = insert(:prm, :division, legal_entity: legal_entity)
-      employee = insert(:prm, :employee)
-      employee_id = employee.id
-      insert(:prm, :contract_division, contract_id: contract.id, division_id: division.id)
-
-      insert(
-        :prm,
-        :contract_employee,
-        contract_id: contract.id,
-        employee_id: employee_id,
-        division_id: division.id,
-        declaration_limit: 2000
-      )
-
-      party_user = insert(:prm, :party_user)
-
-      content = %{
-        "employee_id" => employee_id,
-        "division_id" => division.id,
-        "declaration_limit" => 10,
-        "staff_units" => 0.33
-      }
-
-      params = %{
-        "signed_content" =>
-          content
-          |> Jason.encode!()
-          |> Base.encode64(),
-        "signed_content_encoding" => "base64"
-      }
-
-      drfo_signed_content(content, party_user.party.tax_id)
-
-      conn =
-        conn
-        |> put_client_id_header(UUID.generate())
-        |> put_consumer_id_header(party_user.user_id)
-        |> Plug.Conn.put_req_header("drfo", party_user.party.tax_id)
-        |> patch(contract_path(conn, :update, contract.id), params)
-
-      assert resp = json_response(conn, 422)
-      assert "Employee and contract legal_entity_id mismatch" == resp["error"]["message"]
-    end
-
-    test "succes update employee", %{conn: conn} do
+    test "success update employee", %{conn: conn} do
       nhs()
 
       expect(MediaStorageMock, :store_signed_content, fn _, _, _, _, _ ->
@@ -922,7 +873,7 @@ defmodule EHealth.Web.ContractControllerTest do
 
       conn =
         conn
-        |> put_client_id_header(UUID.generate())
+        |> put_client_id_header(legal_entity.id)
         |> put_consumer_id_header(party_user.user_id)
         |> Plug.Conn.put_req_header("drfo", party_user.party.tax_id)
         |> patch(contract_path(conn, :update, contract.id), params)
@@ -962,7 +913,7 @@ defmodule EHealth.Web.ContractControllerTest do
 
       conn =
         conn
-        |> put_client_id_header(UUID.generate())
+        |> put_client_id_header(legal_entity.id)
         |> put_consumer_id_header(party_user.user_id)
         |> Plug.Conn.put_req_header("drfo", party_user.party.tax_id)
         |> patch(contract_path(conn, :update, contract.id), params)
